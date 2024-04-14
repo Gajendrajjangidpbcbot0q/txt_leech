8231ddcb7730b17151f246989c5e3bec6e86a0f6
Gajendrajjangidpbcbot0q <166974159+Gajendrajjangidpbcbot0q@users.noreply.github.com>
2024-04-14 11:09:01 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "21157244"
+api_hash = "4981c2699bd91c7db836ec8f77e5b0f0"
+bot_token = "6715959692:AAGAqqrUV2yU1rsoJMMgZ_iWlPPPgxt-f10"
